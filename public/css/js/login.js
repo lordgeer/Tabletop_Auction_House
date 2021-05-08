5a69bf351ae8bcd763e0ff019b8d8307159432e3
Thomas G Musler <thomas.g.musler@gmail.com>
2021-05-08 11:39:06 -0400

    added login and logout as well as nominclature

---
+++ public/css/js/login.js
@@ -0,0 +1,28 @@
+const loginFormHandler = async (event) => {
+    // Stop the browser from submitting the form so we can do so with JavaScript
+    event.preventDefault();
+  
+    // Gather the data from the form elements on the page
+    const username = document.querySelector('#username-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+  
+    if (username && password) {
+      // Send the username and password to the server
+      const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({ username, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('Failed to log in');
+      }
+    }
+  };
+  
+  document
+    .querySelector('.login-form')
+    .addEventListener('submit', loginFormHandler);
+  
