cb9b5f6d05911c13799ff16ef2ebc5bf2d69295a
Thomas G Musler <thomas.g.musler@gmail.com>
2021-05-08 11:59:15 -0400

    added user model and adjustments to index and seeds

---
+++ models/user.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+      password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+      },
+    },
+  },
+  {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+);
+
+module.exports = User;
--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -1,17 +1,14 @@
 [
   {
     "name": "Thomas",
-    "email": "thomas@thomas.com",
     "password": "password1234"
   },
   {
     "name": "Elise",
-    "email": "elise@gmail.com",
     "password": "password2021"
   },
   {
     "name": "Dana",
-    "email": "dana@aol.com",
     "password": "dana12345"
   }
 ]
