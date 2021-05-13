f6ab85c0481ed250bb7d144a325e1144c983bb77
Elise Daly <elise.samantha.daly@gmail.com>
2021-05-13 19:36:05 -0400

    Pushing more button changes

--- public/js/shop.js
+++ public/js/shop.js
@@ -15,11 +15,23 @@ function viewItem() {
   var x, i;
   x = document.querySelectorAll(".viewItem-button");
   for (i = 0; i < x.length; i++) {
-    x[i].addEventListener("click", function() {
-      var value = 2;
-      console.log(x[value].attributes[2].nodeValue);
+    x[i].addEventListener("click", function(target) {
+      document.location.replace(`/product/${target.path[0].attributes[2].nodeValue}`);
     });
   }
 }
 
 viewItem();
+
+function deleteItem() {
+  var x, i;
+  x = document.querySelectorAll(".deleteItem-button");
+  for (i = 0; i < x.length; i++) {
+    x[i].addEventListener("click", function(target) {
+      // delete function here
+      console.log(`delete ${target.path[0].attributes[2].nodeValue}`)
+    });
+  }
+}
+
+deleteItem();
