593ce62fc72dca45e540410308db0cd2577b0599
Thomas G Musler <thomas.g.musler@gmail.com>
2021-05-12 21:57:42 -0400

    working renders and updated seed error correction

--- routes/home-routes.js
+++ routes/home-routes.js
@@ -18,12 +18,6 @@ router.get('/shop', async (req, res) => {
   try {
     // Get all projects and JOIN with user data
     const productData = await Product.findAll({
-      // include: [
-      //   {
-      //     model: Product,
-      //     attributes: ['name'],
-      //   },
-      // ],
     });
     console.log(productData);
     // Serialize data so the template can read it
@@ -43,21 +37,10 @@ router.get('/shop', async (req, res) => {
 router.get('/signup', async (req, res) => {
   try {
     const { name, password} = req.body;
-      if (users.find(user => user.name === name)) {
-
-        res.render('signup', {
-          message: 'User already registered.',
-          messageClass: 'alert-danger'
-        });
-
-        return;
-      }
-      users.push({
-        name,
-        password
-      });
+   
+   
       res.render('signup', {
-        products,
+       
         logged_in: req.session.logged_in
       });
     } catch(err) {
@@ -66,13 +49,23 @@ router.get('/signup', async (req, res) => {
     }
   });
 
-  // router.get('/product', async (req, res) => {
-  //   try {
-  //     const productData = await Product.findOne({
-  //   } catch (error) {
-      
-  //   }
-  // })
-
+  router.get('/product', async (req, res) => {
+    try {
+      // Get all projects and JOIN with user data
+      const productData = await Product.findAll({
+      });
+      console.log(productData);
+      // Serialize data so the template can read it
+      const products = productData.map((product) => product.get({ plain: true }));
+      // Pass serialized data and session flag into template
+      res.render('product', {
+        products,
+        logged_in: req.session.logged_in
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(400).json(err);
+    }
+  });
 
   module.exports = router;
