87047feba01e89e1267a02cbe876de30faa436c5
SMakela <73409799+smakela13@users.noreply.github.com>
2021-05-13 18:42:46 -0400

    Removed extra category-seeds that popped in last night, added more products, including missing Star Wars armor

--- seeds/category-seeds.js
+++ seeds/category-seeds.js
@@ -2,38 +2,32 @@ const { Category } = require('../models');
 
 const categoryData = [
   {
-    category_name: 'Weapons',
+    category_name: "Weapons",
   },
   {
-    category_name: 'Armor',
+    category_name: "Armor",
   },
   {
-    category_name: 'Utility',
+    category_name: "Utility",
   },
   {
-    category_name: 'Technology',
+    category_name: "Technology",
   },
   {
-    category_name: 'Companions',
+    category_name: "Companions",
   },
   {
-    category_name: 'Droids',
+    category_name: "Droids",
   },
   {
-    category_name: 'Magic Items',
+    category_name: "Magic Items",
   },
   {
-    category_name: 'Vehicles',
+    category_name: "Vehicles",
   },
   {
-    category_name: 'Companions',
-  },
-  {
-    category_name: 'Collectibles',
-  },
-  {
-    category_name: 'Vehicles',
-  },
+    category_name: "Mounts",
+  }
 ];
 
 const seedCategories = () => Category.bulkCreate(categoryData);
