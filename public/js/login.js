557a29df696fb0a77419aba60c19864d19498840
Elise Daly <elise.samantha.daly@gmail.com>
2021-05-12 20:31:07 -0400

    Adding functionality to signup button

--- public/js/login.js
+++ public/js/login.js
@@ -1,28 +1,38 @@
 const loginFormHandler = async (event) => {
-    // Stop the browser from submitting the form so we can do so with JavaScript
-    event.preventDefault();
-  
-    // Gather the data from the form elements on the page
-    const username = document.querySelector('#username-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
-  
-    if (username && password) {
-      // Send the username and password to the server
-      const response = await fetch('/api/users/login', {
-        method: 'POST',
-        body: JSON.stringify({ username, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-      console.log(response);
-      if (response.ok) {
-        document.location.replace('/shop');
-      } else {
-        alert('Failed to log in');
-      }
+  // Stop the browser from submitting the form so we can do so with JavaScript
+  event.preventDefault();
+
+  // Gather the data from the form elements on the page
+  const username = document.querySelector('#username-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+
+  if (username && password) {
+    // Send the username and password to the server
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      body: JSON.stringify({ username, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    console.log(response);
+    if (response.ok) {
+      document.location.replace('/shop');
+    } else {
+      alert('Failed to log in');
     }
-  };
+  }
+};
+
+const signupButtonScript = async (event) => {
+  event.preventDefault();
+
+  document.location.replace('signup');
+};
   
-  document
-    .querySelector('#login-form')
-    .addEventListener('submit', loginFormHandler);
+document
+  .querySelector('#login-form')
+  .addEventListener('submit', loginFormHandler)
+  
+  .querySelector('#signup-form')
+  .addEventListener('click', signupButtonScript);
+    
   
