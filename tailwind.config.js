7c5ffb6d7045f6628b80c9e5bdc32431b941c2a0
SMakela <73409799+smakela13@users.noreply.github.com>
2021-05-14 16:40:25 -0400

    Corrected the categories on view.handlebars and product.handlebars to remove the duplicates, added CSS to the login.handlebars page and main.handlebars page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,17 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      // Configure your color palette here
+      blue: {
+        light: '#6679B6',
+        dark: '#0B1C51',
+        grey: '#A8B0C8',
+      },
+      gold: {
+        light: '#A68E72'
+      }
+    },
     extend: {},
   },
   variants: {
