524baeab110ae56a76e49136b9c5d1e0f55c56b7
Elise Daly <elise.samantha.daly@gmail.com>
2021-05-15 15:01:09 -0400

    Adding Category to View Handlebars

--- public/js/view-item.js
+++ public/js/view-item.js
@@ -31,3 +31,33 @@ function deleteItem() {
 }
 
 deleteItem();
+
+switch(document.getElementById("item-category").attributes[3].nodeValue) {
+  case "1":
+    document.getElementById("item-category").innerHTML = "Weapons"
+    break;
+  case "2":
+    document.getElementById("item-category").innerHTML = "Armor"
+    break;
+  case "3":
+    document.getElementById("item-category").innerHTML = "Utility"
+    break;
+  case "4":
+    document.getElementById("item-category").innerHTML = "Technology"
+    break;
+  case "5":
+    document.getElementById("item-category").innerHTML = "Companions"
+    break;
+  case "6":
+    document.getElementById("item-category").innerHTML = "Droids"
+    break;
+  case "7":
+    document.getElementById("item-category").innerHTML = "Magic Items"
+    break;
+  case "8":
+    document.getElementById("item-category").innerHTML = "Vehicles"
+    break;
+  case "9":
+    document.getElementById("item-category").innerHTML = "Mounts"
+    break;
+}
